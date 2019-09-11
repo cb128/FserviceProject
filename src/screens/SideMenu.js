34b1b5b14c400782ef1a7ec01a7dacfc5cb8515a
Nguyen Cao Thang <caothang@trycz.vn>
2019-09-11 14:54:15 +0700

    Add toggle menu button.

--- src/screens/SideMenu.js
+++ src/screens/SideMenu.js
@@ -6,6 +6,7 @@ import {
   Alert,
   ScrollView,
   StyleSheet,
+  Dimensions,
 } from 'react-native';
 import {Icon, Image, Divider} from 'react-native-elements';
 import AsyncStorage from '@react-native-community/async-storage';
@@ -92,20 +93,36 @@ class SideMenu extends React.Component {
     this.props.navigation.navigate('ChangePassword');
   };
 
+  _closeMenu = () => {
+    this.props.navigation.closeDrawer();
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <ScrollView>
           <View style={styles.headerView}>
-            <Image
-              source={
-                this.state.avatarPath.includes('http')
-                  ? {uri: this.state.avatarPath}
-                  : placeHolderImage
-              }
-              style={styles.imageView}
-            />
-            <Text style={styles.username}>{this.state.name}</Text>
+            <TouchableOpacity
+              onPress={this._closeMenu}
+              style={styles.toggleView}>
+              <Icon
+                name="menu"
+                type="material"
+                color="black"
+                containerStyle={styles.toggleButton}
+              />
+            </TouchableOpacity>
+            <View style={styles.contentView}>
+              <Image
+                source={
+                  this.state.avatarPath.includes('http')
+                    ? {uri: this.state.avatarPath}
+                    : placeHolderImage
+                }
+                style={styles.imageView}
+              />
+              <Text style={styles.username}>{this.state.name}</Text>
+            </View>
           </View>
           <TouchableOpacity style={styles.menuItem} onPress={this._goToProfile}>
             <Icon
@@ -159,6 +176,17 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
   headerView: {
+    backgroundColor: '#ffb900',
+  },
+  toggleView: {
+    height: 30,
+  },
+  toggleButton: {
+    marginTop: 10,
+    marginRight: 10,
+    alignItems: 'flex-end',
+  },
+  contentView: {
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: '#ffb900',
